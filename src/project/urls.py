ad8607dcef0d19e25ecd6cb5ac4fe3101c2f2e08
oluochelvis <oduorelvis247@gmail.com>
2018-06-01 00:03:22 +0300

    added urls to board

--- src/project/urls.py
+++ src/project/urls.py
@@ -20,5 +20,6 @@ from project.boards import views
 
 urlpatterns = [
     url(r'^$', views.boards_home, name='boards_home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^admin/', admin.site.urls),
 ]
