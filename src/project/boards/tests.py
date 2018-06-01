fc5c4a7eb40c12cfea0098a1026fd1d56de4b9fe
oluochelvis <oduorelvis247@gmail.com>
2018-06-01 17:14:42 +0300

    sign up

--- src/project/boards/tests.py
+++ src/project/boards/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from .views import boards_home, board_topics, new_topic
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 
 class HomeTests(TestCase):
--- src/project/urls.py
+++ src/project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from project.accounts import views as accounts_views
 from project.boards import views
 
 urlpatterns = [
     url(r'^$', views.boards_home, name='boards_home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
